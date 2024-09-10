42a33913ae19332c5aa2d55baf3d438666edcf79
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-09-10 17:57:36 -0300

    banco de dados concluido

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,12 @@
 import fastify from "fastify";
 
-const app = fastify()
+const app = fastify();
 
-app.listen({
-
-    port: 3333,
-
-}).then(() => {
-    console.log("http server running!")
-
-})
+app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("http server running!");
+	});
+    
