b248ddfa622ebce83bb2321b3ddb1a114d10850b
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-09-11 17:53:28 -0300

    inserindo o cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,41 +1,33 @@
-import fastify from "fastify";
-import { serializerCompiler,
-	 validatorCompiler,
-	type ZodTypeProvider,
- } from "fastify-type-provider-zod";
-import { createGoal } from "../db/functions/create_goal";
-import z from 'zod'
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
-
-app.post('/goals', {
-schema: {
-	body: z.object({
-title: z.string(),
-desiredWeeklyFrequency: z.number().int().min(1).max(7),
-}),
-}
-
-},	async request => {
-const { title, desiredWeeklyFrequency} = request.body
-	
-const createGoalSchema = 
-await createGoal({
-title,
-desiredWeeklyFrequency,
-
+app.register(fastifyCors, {
+  origin: '*',
 })
 
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
-	.listen({
-		port: 3333,
-	})
-	.then(() => {
-		console.log("http server running!");
-	});
-    
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
